03a99c240afd6fc0bf741f9c36e5bceaf46ae35d
Sasha <64744993+r1tsuu@users.noreply.github.com>
2024-10-25 21:04:07 +0300

    before commit

--- test/relationships/int.spec.ts
+++ test/relationships/int.spec.ts
@@ -875,6 +875,50 @@ describe('Relationships', () => {
         expect(query.docs[0].id).toStrictEqual(firstLevelID)
       })
 
+      it('should allow querying several times, one and two level deep', async () => {
+        let query
+        const now = Date.now()
+        for (let i = 0; i < 100; i++) {
+          query = await payload.find({
+            collection: 'chained',
+            where: {
+              and: [
+                {
+                  'relation.name': {
+                    equals: 'second',
+                  },
+                },
+                {
+                  'relation.relation.name': {
+                    equals: 'third',
+                  },
+                },
+                {
+                  'relation.relation.name': {
+                    like: 'third',
+                  },
+                },
+                {
+                  'relation.relation.name': {
+                    exists: true,
+                  },
+                },
+                {
+                  'relation.relation.name': {
+                    not_equals: 'third1',
+                  },
+                },
+              ],
+            },
+          })
+        }
+
+        payload.logger.info(`Benchmark end, execution time: ${Date.now() - now}MS`)
+
+        expect(query.docs).toHaveLength(1)
+        expect(query.docs[0].id).toStrictEqual(firstLevelID)
+      })
+
       it('should allow querying within array nesting', async () => {
         const page = await payload.create({
           collection: 'pages',
